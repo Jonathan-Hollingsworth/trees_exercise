d35a5586b7e255e335589c1796921612971bb50b
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-11-17 16:40:11 -0500

    Created and tested functions for Tree

--- tree.js
+++ tree.js
@@ -15,20 +15,69 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    let toVisitStack = [this.root];
+    let sum = 0
+
+    while (toVisitStack.length) {
+      let current = toVisitStack.pop();
+
+      if (current !== null) {
+        console.log(current.val)
+        sum += current.val;
+      }
+
+      if (current !== null) {
+        for (let child of current.children) 
+        toVisitStack.push(child)
+      }
+    }
+
+    return sum
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    let toVisitStack = [this.root];
+    let count = 0
 
+    while (toVisitStack.length) {
+      let current = toVisitStack.pop();
+
+      if (current !== null && current.val % 2 === 0) {
+        count ++;
+      }
+
+      if (current !== null) {
+        for (let child of current.children) 
+        toVisitStack.push(child)
+      }
+    }
+
+    return count
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    let toVisitStack = [this.root];
+    let count = 0
 
+    while (toVisitStack.length) {
+      let current = toVisitStack.pop();
+
+      if (current !== null && current.val > lowerBound) {
+        count ++;
+      }
+
+      if (current !== null) {
+        for (let child of current.children) 
+        toVisitStack.push(child)
+      }
+    }
+
+    return count
   }
 }
 
